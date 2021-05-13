8e6acc3bd7c026bbfabe8d18194f6f9b6d4b15ce
Etienne Parent <tricky@tricky-dev>
2021-05-13 20:11:01 +0000

    In progress: working on the recorder

--- src/recorder.js
+++ src/recorder.js
@@ -1,72 +1,70 @@
-const puppeteer = require('puppeteer');
-const PuppeteerMassScreenshots = require('puppeteer-mass-screenshots');
-
-export function RecordCodeCell(pageURL, videosPath, cell) {
-    // Cell is an int for which code cell to execute
-    (async () => {
-        const browser = await puppeteer.launch({ headless: true });
-        const page = (await browser.pages())[0];
-        const screenshots = new PuppeteerMassScreenshots();
-        await screenshots.init(page, videosPath);
-        await page.goto(pageURL);
-        await screenshots.start();
-        // await page.waitForNavigation({ waitUntil: 'domcontentloaded'});
-        const input = await page.$('input[name=q]');
-        await input.type('puppetter-mass-screenshots', { delay: 250 });
-        await input.press('Enter');
-        // await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
-        await screenshots.stop();
-        await browser.close();
-    })();
-}
+const puppeteer = require("puppeteer");
+const PuppeteerMassScreenshots = require("puppeteer-mass-screenshots");
 
 async function getAllCells(page) {
-        // page is an object created by the browser.
-        const cells = await page.evaluate(() => {
-                return document.getElementsByClassName("cell")
-        });
-        return cells
+  // page is an object created by the browser.
+  const cells = await page.evaluate(() => {
+    return document.getElementsByClassName("cell");
+  });
+  return cells;
 }
 
 function isTextCell(cell) {
-        // cell should be an element of the array returned by
-        // GetAllCells
-        var allClasses = cell.classList();
-        if ("text_cell" in allClasses) {
-                return true
-        } else {
-                return false
+  // cell should be an element of the array returned by
+  // GetAllCells
+  var allClasses = cell.classList;
+  if (allClasses.contains("text_cell")) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+async function scrollTo(page, secondCell) {
+  await page.evaluate(() => {
+    secondCell.scrollIntoView();
+  });
+}
+
+async function unselectAll(page) {
+    await page.evaluate(() => {
+        cells = document.getElementsByClassName("cell");
+        for (let k=0; k<cells.length; k++) {
+            cells[k].classList.remove("selected");
         }
+    }
 }
 
-async function scrollTo(page, firstCell, secondCell) {
-
-
-}
-
-function recordTransition(page, videoPath, firstCell, secondCell) {
-        // Use scroll function
-        // Scroll to the right place
-        // Record the scrolling @60 fps
-        // Stop recording
+async function recordTransition(page, videoPath, firstCell, secondCell) {
+  (async () => {
+    await screenshots.init(page, videosPath);
+    await screenshots.start();
+    await scrollTo(page, secondCell);
+    await screenshots.stop();
+    await browser.close();
+  })();
 }
 
 function recordCodeCell(page, videoPath, cell) {
-        // Start recording @60 fps
-        // Run code cell
-        // Stop recording
+  (async () => {
+    await screenshots.init(page, videosPath);
+    await screenshots.start();
+    await scrollTo(page, secondCell);
+    await screenshots.stop();
+    await browser.close();
+  })();
 }
 
 function recordTextCell(page, videoPath, cell) {
-        // Take a screenshot
+  // Take a screenshot
 }
 
-export async function RecordNotebook(pageURL, savePath){
-
-        // Loading page contents
-        const browser = await puppeteer.launch({ headless: true});
-        const page = (await browser.pages())[0];
-        page.goto(pageURL);
-        var allCells = GetAllCells(page);
-        allCells.forEach(recordCell);
+export async function RecordNotebook(pageURL, savePath) {
+  // Loading page contents
+  const browser = await puppeteer.launch({ headless: true });
+  const page = (await browser.pages())[0];
+  const screenshots = new PuppeteerMassScreenshots();
+  page.goto(pageURL);
+  var allCells = GetAllCells(page);
+  allCells.forEach(recordCell);
 }
