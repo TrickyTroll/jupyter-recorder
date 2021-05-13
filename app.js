16503f381e8ca795d6cf306b29faad04e10623a9
Etienne Parent <tricky@tricky-dev>
2021-05-13 20:10:22 +0000

    Style: formatted files

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 
-require = require('esm')(module);
-const screenshot = require('./src/screenshot');
-const recorder = require('./src/recorder');
+require = require("esm")(module);
+const screenshot = require("./src/screenshot");
+const recorder = require("./src/recorder");
 
 var args = process.argv.slice(2); // Removing 'node' and the name
-                                  // of the program.
+// of the program.
 
-console.log(`Running with args ${args}`)
+console.log(`Running with args ${args}`);
 recorder.Record(args[0], args[1]);
