9678c50f24c4685f95b746ba5de6e1729c3132c4
TrickyTroll <53831454+TrickyTroll@users.noreply.github.com>
2021-05-19 14:06:55 -0400

    Change: converted to es6 module

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 
 require = require("esm")(module);
-const screenshot = require("./src/screenshot");
-const recorder = require("./src/recorder");
+import screenshot from "./src/screenshot";
+import { recordAllCode } from "./src/recorder";
 
 var args = process.argv.slice(2); // Removing 'node' and the name
 // of the program.
 
 console.log(`Running with args ${args}`);
-recorder.recordAllCode(args[0], args[1]);
+recordAllCode(args[0], args[1]);
