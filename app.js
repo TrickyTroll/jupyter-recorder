2c4125c79c8f59b3294b508b14429611fbf5c73d
TrickyTroll <53831454+TrickyTroll@users.noreply.github.com>
2021-05-21 08:24:34 -0400

    In progress: `sync` functions

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 import { recordAllCode } from './src/recorder.js';
-import { getFilePath, getServerChoice } from './src/cli.cjs'
+import { getFilePath, getServerChoice } from './src/cli.js';
 
-const server = getServerChoice();
-const savePath = getFilePath();
 
-recordAllCode(server, savePath);
+const server = await getServerChoice();
+//const savePath = getFilePath();
+
+// recordAllCode(server, savePath);
